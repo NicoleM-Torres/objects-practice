482bde8cfc9857f3b429f6163698d6502deaf0e7
Nicole Torres <nicole.torres@mscoding.org>
2024-08-14 13:07:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 //OBJECTS EXAMPLES
 
 let favCharacter = {
-    fName : "Bob",
-    lName : "Ross",
-    age: "52 -- dead",
-    occupation: "artist",
-    hobbies: ["film wildlife", "help injured animals recover", "smoke"],
-    currentHealth: "unalived",
-    
-}
+  fName: "Bob",
+  lName: "Ross",
+  age: "52 -- dead",
+  occupation: "artist",
+  hobbies: ["film wildlife", "help injured animals recover", "smoking"],
+  currentHealth: "unalived",
+};
 
-console.log (favCharacter);
+//CALLS ENTIRE OBJECT & PROPERTIES
+console.log(favCharacter);
+//CALLING PROPERTY TYPE VALUES
+console.log(
+  `My fav characters name is ${favCharacter.fName} ${favCharacter.lName}`
+);
+//CALLING PROPERTY TYPE THAT HAS AN ARRAY
+console.log(
+  `One of my favorite characters hobbies is ${favCharacter.hobbies[2]}`
+);
+
+//UPDATE PROPERTY VALUE
+favCharacter.currentHealth = "Haunting others";
+console.log (favCharacter.currentHealth);
+
+//ADDING A NEW  PROPERTY TO THE OBJECT VARIABLE
+favCharacter.paintingSkills= "Alla prima painting technique";
+console.log (favCharacter)
+
