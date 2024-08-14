9cab688dd33866902e4068340e1e0aab520811f8
Nicole Torres <nicole.torres@mscoding.org>
2024-08-14 11:08:21 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//OBJECTS EXAMPLES
+
+let favCharacter = {
+    fName : "Bob",
+    lName : "Ross",
+    age: "52 -- dead",
+    occupation: "artist",
+    hobbies: ["film wildlife", "help injured animals recover", "smoke"],
+    currentHealth: "unalived",
+    
+}
+
+console.log (favCharacter);
